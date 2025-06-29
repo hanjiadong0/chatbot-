42ecf2578f568dfcbffafb49389fe57e6b593946
hanjiadong0 <38018421+hanjiadong0@users.noreply.github.com>
2025-06-30 00:19:34 +0200

    Add files via upload

---
+++ thesis_assistant/rl_agent/rl_components.py
@@ -0,0 +1,44 @@
+# Necessary imports will need to be added manually or refined.
+
+# Code for the following components should go here:
+# - RLConfigManager
+# - DataPreprocessor
+# - MockEthicsModule
+# - EthicsSupervisorEnv
+# - EthicsSupervisorRL
+# - RLTrainingLoop
+# - ThesisStudentSimulator
+# - ThesisCohortSimulator
+# - SyntheticRLPretrainer
+# - RLTrainingLauncher
+
+# Could not automatically extract source for RLConfigManager due to error: source code not available
+# Please manually copy the code for RLConfigManager from the notebook.
+
+# Could not automatically extract source for DataPreprocessor due to error: source code not available
+# Please manually copy the code for DataPreprocessor from the notebook.
+
+# Could not automatically extract source for MockEthicsModule due to error: source code not available
+# Please manually copy the code for MockEthicsModule from the notebook.
+
+# Could not automatically extract source for EthicsSupervisorEnv due to error: source code not available
+# Please manually copy the code for EthicsSupervisorEnv from the notebook.
+
+# Could not automatically extract source for EthicsSupervisorRL due to error: source code not available
+# Please manually copy the code for EthicsSupervisorRL from the notebook.
+
+# Could not automatically extract source for RLTrainingLoop due to error: source code not available
+# Please manually copy the code for RLTrainingLoop from the notebook.
+
+# Could not automatically extract source for ThesisStudentSimulator due to error: source code not available
+# Please manually copy the code for ThesisStudentSimulator from the notebook.
+
+# Could not automatically extract source for ThesisCohortSimulator due to error: source code not available
+# Please manually copy the code for ThesisCohortSimulator from the notebook.
+
+# Could not automatically extract source for SyntheticRLPretrainer due to error: source code not available
+# Please manually copy the code for SyntheticRLPretrainer from the notebook.
+
+# Could not automatically extract source for RLTrainingLauncher due to error: source code not available
+# Please manually copy the code for RLTrainingLauncher from the notebook.
+
